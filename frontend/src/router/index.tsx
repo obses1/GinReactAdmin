5f211b9ccf6d9a1cd27f17a61373e4d0fbf2cfb4
Han Bingda <3446629743@qq.com>
2026-07-14 18:40:34 +0800

    fix-router-lazy-import

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
+import Dashboard from '../pages/Dashboard'
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +9,7 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        lazy: () => import('../pages/Dashboard'),
+        element: <Dashboard />,
       },
     ],
   },
